56059eb0fe82fcf99247aa2fffcd68f07aef331d
Michael Nairn <mnairn@redhat.com>
2023-03-21 09:48:51 +0000

    Generate kustomize configs per cluster during local setup
    Update the local-setup logic to generate a set of kustomize configs for
    each kind cluster (control, or workload) being created during local
    setup. These configs use one of two different overlays (control-cluster
    or workload-cluster) depending on what type of cluster is being created,
    and this overlay is responsible for maintaining the set of resources
    that need to be added to that type of cluster.  Kustomize now manages
    the creation of each clusters secret used by both argocd and the mctc
    controller/agent/syncer itself. During generation of the clusters config
    a `cluster-secret` kustomization is generated containing config
    extracted from the running clusters kubeconfig. Note the `server` for
    these secrets now use the internal node ips instead for the mctc
    network, so there is no need for 2 secrets (internal/external).

--- hack/local-setup.sh
+++ hack/local-setup.sh
@@ -18,6 +18,7 @@
 
 LOCAL_SETUP_DIR="$(dirname "${BASH_SOURCE[0]}")"
 source "${LOCAL_SETUP_DIR}"/.setupEnv
+source "${LOCAL_SETUP_DIR}"/.startUtils
 source "${LOCAL_SETUP_DIR}"/.kindUtils
 source "${LOCAL_SETUP_DIR}"/.cleanupUtils
 
@@ -25,30 +26,13 @@ KIND_CLUSTER_PREFIX="mctc-"
 KIND_CLUSTER_CONTROL_PLANE="${KIND_CLUSTER_PREFIX}control-plane"
 KIND_CLUSTER_WORKLOAD="${KIND_CLUSTER_PREFIX}workload"
 
-INGRESS_NGINX_KUSTOMIZATION_DIR=${LOCAL_SETUP_DIR}/../config/ingress-nginx
 METALLB_KUSTOMIZATION_DIR=${LOCAL_SETUP_DIR}/../config/metallb
-CERT_MANAGER_KUSTOMIZATION_DIR=${LOCAL_SETUP_DIR}/../config/cert-manager
-EXTERNAL_DNS_KUSTOMIZATION_DIR=${LOCAL_SETUP_DIR}/../config/external-dns
-ARGOCD_KUSTOMIZATION_DIR=${LOCAL_SETUP_DIR}/../config/argocd
-ISTIO_KUSTOMIZATION_DIR=${LOCAL_SETUP_DIR}/../config/istio/istio-operator.yaml
-GATEWAY_API_KUSTOMIZATION_DIR=${LOCAL_SETUP_DIR}/../config/gateway-api
-WEBHOOK_PATH=${LOCAL_SETUP_DIR}/../config/webhook-setup/workload
-TLS_CERT_PATH=${LOCAL_SETUP_DIR}/../config/webhook-setup/control/tls
 
 LOCAL_SETUP_CONFIG_DIR=${LOCAL_SETUP_DIR}/../config/local-setup
 LOCAL_SETUP_CLUSTERS_DIR=${LOCAL_SETUP_CONFIG_DIR}/clusters
 LOCAL_SETUP_CLUSTERS_TMPL_DIR=${LOCAL_SETUP_CONFIG_DIR}/cluster-templates
 
 set -e pipefail
-
-deployIngressController () {
-  clusterName=${1}
-  kubectl config use-context kind-${clusterName}
-  echo "Deploying Ingress controller to ${clusterName}"
-  ${KUSTOMIZE_BIN} build ${INGRESS_NGINX_KUSTOMIZATION_DIR} --enable-helm --helm-command ${HELM_BIN} | kubectl apply -f -
-  echo "Waiting for deployments to be ready ..."
-  kubectl -n ingress-nginx wait --timeout=300s --for=condition=Available deployments --all
-}
 
 deployMetalLB () {
   clusterName=${1}
@@ -78,82 +62,6 @@ metadata:
 EOF
 }
 
-deployCertManager() {
-  clusterName=${1}
-  echo "Deploying Cert Manager to (${clusterName})"
-
-  kubectl config use-context kind-${clusterName}
-
-  ${KUSTOMIZE_BIN} build ${CERT_MANAGER_KUSTOMIZATION_DIR} --enable-helm --helm-command ${HELM_BIN} | kubectl apply -f -
-  echo "Waiting for Cert Manager deployments to be ready..."
-  kubectl -n cert-manager wait --timeout=300s --for=condition=Available deployments --all
-
-  kubectl delete validatingWebhookConfiguration mctc-cert-manager-webhook
-  kubectl delete mutatingWebhookConfiguration mctc-cert-manager-webhook
-  # Apply the default glbc-ca issuer
-  kubectl create -n cert-manager -f ./config/default/issuer.yaml
-}
-
-deployExternalDNS() {
-  clusterName=${1}
-  echo "Deploying ExternalDNS to (${clusterName})"
-
-  kubectl config use-context kind-${clusterName}
-
-  ${KUSTOMIZE_BIN} build ${EXTERNAL_DNS_KUSTOMIZATION_DIR} --enable-helm --helm-command ${HELM_BIN} | kubectl apply -f -
-  echo "Waiting for External DNS deployments to be ready..."
-  kubectl -n external-dns wait --timeout=300s --for=condition=Available deployments --all
-}
-
-deployArgoCD() {
-  clusterName=${1}
-  echo "Deploying ArgoCD to (${clusterName})"
-
-  kubectl config use-context kind-${clusterName}
-
-  ${KUSTOMIZE_BIN} build ${ARGOCD_KUSTOMIZATION_DIR} --enable-helm --helm-command ${HELM_BIN} | kubectl apply -f -
-  echo "Waiting for ARGOCD deployments to be ready..."
-  kubectl -n argocd wait --timeout=300s --for=condition=Available deployments --all
-
-  ports=$(docker ps --format '{{json .}}' | jq "select(.Names == \"$clusterName-control-plane\").Ports")
-  httpsport=$(echo $ports | sed -e 's/.*0.0.0.0\:\(.*\)->443\/tcp.*/\1/')
-  argoPassword=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
-  nodeIP=$(kubectl get nodes -o json | jq -r ".items[] | select(.metadata.name == \"$clusterName-control-plane\").status | .addresses[] | select(.type == \"InternalIP\").address")
-
-  echo -ne "\n\n\tConnect to ArgoCD UI\n\n"
-  echo -ne "\t\tLocal URL: https://argocd.127.0.0.1.nip.io:$httpsport\n"
-  echo -ne "\t\tNode URL : https://argocd.$nodeIP.nip.io\n"
-  echo -ne "\t\tUser     : admin\n"
-  echo -ne "\t\tPassword : $argoPassword\n\n\n"
-}
-
-deployIstio() {
-  clusterName=${1}
-
-  echo "Deploying Istio to (${clusterName})"
-
-  ${ISTIOCTL_BIN} operator init
-  kubectl apply -f  ${ISTIO_KUSTOMIZATION_DIR}
-}
-
-installGatewayAPI() {
-  clusterName=${1}
-
-  echo "Installing Gateway API in ${clusterName}"
-
-  kubectl config use-context kind-${clusterName}
-
-  ${KUSTOMIZE_BIN} build ${GATEWAY_API_KUSTOMIZATION_DIR} | kubectl apply -f -
-}
-
-deployKuadrant(){
-  clusterName=${1}
-  kubectl config use-context kind-${clusterName}
-
-  echo "Installing Kuadrant in ${clusterName}"
-  ${KUSTOMIZE_BIN} build config/kuadrant | kubectl apply -f -
-}
-
 deployDashboard() {
   clusterName=${1}
   portOffset=${2}
@@ -175,15 +83,6 @@ deployDashboard() {
   echo -ne "\n\n\tAccess Kubernetes Dashboard\n\n"
   echo -ne "\t\t\t* The dashboard is available at http://localhost:$port/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/\n"
   echo -ne "\t\tAccess the dashboard using the following Bearer Token: $token\n"
-}
-
-deployWebhookConfigs(){
-  clusterName=${1}
-  echo "Deploying the webhook configuration to (${clusterName})"
-
-  kubectl config use-context kind-${clusterName}
-
-  kubectl apply -f $WEBHOOK_PATH/webhook-configs.yaml
 }
 
 # Generates a kubeconfig for the given cluster. The server is changed to use the mctc network internal node ip which is
@@ -272,13 +171,33 @@ applyClusterConfig() {
     clusterDir=${LOCAL_SETUP_CLUSTERS_DIR}/${clusterName}
     kubectl config use-context kind-${clusterName}
     echo "Apply cluster config for ${clusterName}"
-    #Apply anything in the init config first
-    ${KUSTOMIZE_BIN} --load-restrictor LoadRestrictionsNone build ${clusterDir}/init --enable-helm --helm-command ${HELM_BIN} | kubectl apply -f -
+    #Apply all config for the cluster
+    #Crude way of making sure everything gets applied eventually. Eventually we might want to break this into separate
+    # phases of deployment to allow dependencies such as CRDs to be installed first.
+    wait_for "${KUSTOMIZE_BIN} --load-restrictor LoadRestrictionsNone build ${clusterDir} --enable-helm --helm-command ${HELM_BIN} | kubectl apply -f -" "${clusterName} cluster config apply" "1m" "5"
     # Wait for all expected deployments to be ready
     echo "Waiting for deployments to be ready ..."
     kubectl -n ingress-nginx wait --timeout=300s --for=condition=Available deployments --all
-    #Apply all config for the cluster
-    ${KUSTOMIZE_BIN} --load-restrictor LoadRestrictionsNone build ${clusterDir} --enable-helm --helm-command ${HELM_BIN} | kubectl apply -f -
+}
+
+waitControlClusterDeploymentsReady() {
+      clusterName=${1}
+      kubectl config use-context kind-${clusterName}
+      # Wait for all expected deployments to be ready
+      echo "Waiting for nginx deployments to be ready ..."
+      kubectl -n ingress-nginx wait --timeout=300s --for=condition=Available deployments --all
+      echo "Waiting for Cert Manager deployments to be ready..."
+      kubectl -n cert-manager wait --timeout=300s --for=condition=Available deployments --all
+      echo "Waiting for ARGOCD deployments to be ready..."
+      kubectl -n argocd wait --timeout=300s --for=condition=Available deployments --all
+}
+
+waitWorkloadClusterDeploymentsReady() {
+      clusterName=${1}
+      kubectl config use-context kind-${clusterName}
+      # Wait for all expected deployments to be ready
+      echo "Waiting for nginx deployments to be ready ..."
+      kubectl -n ingress-nginx wait --timeout=300s --for=condition=Available deployments --all
 }
 
 printClusterInfo() {
@@ -309,55 +228,38 @@ docker network create -d bridge --subnet 172.32.0.0/16 mctc --gateway 172.32.0.1
   -o "com.docker.network.bridge.enable_ip_masquerade"="true" \
   -o "com.docker.network.driver.mtu"="1500"
 
-### Control Cluster ###
+### Create Control Cluster And Generate Config ###
 
 kindCreateCluster ${KIND_CLUSTER_CONTROL_PLANE} ${port80} ${port443}
 generateControlClusterConfig ${KIND_CLUSTER_CONTROL_PLANE}
 
-#2. Install the Gateway API CRDs in the control cluster
-installGatewayAPI ${KIND_CLUSTER_CONTROL_PLANE}
+### Create Workload Clusters And Generate Config ###
 
-#3. Deploy ingress controller
-deployIngressController ${KIND_CLUSTER_CONTROL_PLANE}
-
-#4. Deploy cert manager
-deployCertManager ${KIND_CLUSTER_CONTROL_PLANE}
-
-#5. Deploy argo cd
-deployArgoCD ${KIND_CLUSTER_CONTROL_PLANE}
-
-#6. Deploy Dashboard
-deployDashboard $KIND_CLUSTER_CONTROL_PLANE 0
-
-### Workload Clusters ###
-
-# Create workload Kind clusters if MCTC_WORKLOAD_CLUSTERS_COUNT environment variable is set
 if [[ -n "${MCTC_WORKLOAD_CLUSTERS_COUNT}" ]]; then
   for ((i = 1; i <= ${MCTC_WORKLOAD_CLUSTERS_COUNT}; i++)); do
     kindCreateCluster ${KIND_CLUSTER_WORKLOAD}-${i} $((${port80} + ${i})) $((${port443} + ${i}))
-  done
-fi
-
-# Generate workload cluster configs if MCTC_WORKLOAD_CLUSTERS_COUNT environment variable is set
-if [[ -n "${MCTC_WORKLOAD_CLUSTERS_COUNT}" ]]; then
-  for ((i = 1; i <= ${MCTC_WORKLOAD_CLUSTERS_COUNT}; i++)); do
     generateWorkloadClusterConfig ${KIND_CLUSTER_WORKLOAD}-${i} ${KIND_CLUSTER_CONTROL_PLANE}
   done
 fi
 
-# Apply all cluster configs to local Kind clusters
+### Apply Cluster Configurations ###
+
+# Control Cluster
+applyClusterConfig ${KIND_CLUSTER_CONTROL_PLANE}
+waitControlClusterDeploymentsReady ${KIND_CLUSTER_CONTROL_PLANE}
+deployDashboard $KIND_CLUSTER_CONTROL_PLANE 0
+
+# Workload Clusters
 if [[ -n "${MCTC_WORKLOAD_CLUSTERS_COUNT}" ]]; then
   for ((i = 1; i <= ${MCTC_WORKLOAD_CLUSTERS_COUNT}; i++)); do
-    kindCreateCluster ${KIND_CLUSTER_WORKLOAD}-${i} $((${port80} + ${i})) $((${port443} + ${i}))
     applyClusterConfig ${KIND_CLUSTER_WORKLOAD}-${i}
-    deployIstio ${KIND_CLUSTER_WORKLOAD}-${i}
-    installGatewayAPI ${KIND_CLUSTER_WORKLOAD}-${i}
-    deployIngressController ${KIND_CLUSTER_WORKLOAD}-${i}
+    waitWorkloadClusterDeploymentsReady ${KIND_CLUSTER_WORKLOAD}-${i}
     deployMetalLB ${KIND_CLUSTER_WORKLOAD}-${i} $((${metalLBSubnetStart} + ${i} - 1))
-    deployKuadrant ${KIND_CLUSTER_WORKLOAD}-${i}
     deployDashboard ${KIND_CLUSTER_WORKLOAD}-${i} ${i}
   done
 fi
+
+### Print Cluster Info ###
 
 printClusterInfo $KIND_CLUSTER_CONTROL_PLANE
 
