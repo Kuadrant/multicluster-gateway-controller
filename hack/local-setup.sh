2849c97b9e383fec2dd7b5f8af21324a35fb3906
R-Lawton <rlawton@redhat.com>
2023-05-04 14:37:45 +0100

    comment out old kuadrant

--- hack/local-setup.sh
+++ hack/local-setup.sh
@@ -306,7 +306,6 @@ if [[ -n "${MCTC_WORKLOAD_CLUSTERS_COUNT}" ]]; then
     installGatewayAPI ${KIND_CLUSTER_WORKLOAD}-${i}
     deployIngressController ${KIND_CLUSTER_WORKLOAD}-${i}
     deployMetalLB ${KIND_CLUSTER_WORKLOAD}-${i} $((${metalLBSubnetStart} + ${i} - 1))
-    deployKuadrant ${KIND_CLUSTER_WORKLOAD}-${i}
     deployDashboard ${KIND_CLUSTER_WORKLOAD}-${i} ${i}
     argocdAddCluster ${KIND_CLUSTER_CONTROL_PLANE} ${KIND_CLUSTER_WORKLOAD}-${i}
     deployAgentSecret ${KIND_CLUSTER_WORKLOAD}-${i} "true"
