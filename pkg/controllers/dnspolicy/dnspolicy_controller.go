philbrookes/multi-cluster-traffic-controller

event mapping for DNSHealthProbes on DNSPolicy

pkg/controllers/dnspolicy/dnspolicy_controller.go
@@ -229,6 +229,10 @@ func (r *DNSPolicyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	gatewayEventMapper := events.NewGatewayEventMapper(r.Logger(), &DNSPolicyRefsConfig{}, "dnspolicy")
 	clusterEventMapper := events.NewClusterEventMapper(r.Logger(), r.Client(), &DNSPolicyRefsConfig{}, "dnspolicy")
 	r.dnsHelper = dnsHelper{Client: r.Client()}
+	probeEventMapper := &ProbeEventMapper{
+		Logger: r.Logger().WithName("probeEventMapper"),
+		client: r.Client(),
+	}
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.DNSPolicy{}).
 		Watches(
@@ -238,6 +242,10 @@ func (r *DNSPolicyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Watches(
 			&source.Kind{Type: &clusterv1.ManagedCluster{}},
 			handler.EnqueueRequestsFromMapFunc(clusterEventMapper.MapToPolicy),
+		).
+		Watches(
+			&source.Kind{Type: &v1alpha1.DNSHealthCheckProbe{}},
+			handler.EnqueueRequestsFromMapFunc(probeEventMapper.MapToDNSPolicy),
 		).
 		Complete(r)
 }

Kuadrant/multicluster-gateway-controller

pkg/controllers/dnspolicy/dnspolicy_controller.go
@@ -290,6 +290,7 @@ func (r *DNSPolicyReconciler) updateGatewayCondition(ctx context.Context, condit
 func (r *DNSPolicyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	gatewayEventMapper := events.NewGatewayEventMapper(r.Logger(), &DNSPolicyRefsConfig{}, "dnspolicy")
 	clusterEventMapper := events.NewClusterEventMapper(r.Logger(), r.Client(), &DNSPolicyRefsConfig{}, "dnspolicy")
+	probeEventMapper := events.NewProbeEventMapper(r.Logger(), DNSPolicyBackRefAnnotation, "dnspolicy")
 	r.dnsHelper = dnsHelper{Client: r.Client()}
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&v1alpha1.DNSPolicy{}).
@@ -300,6 +301,10 @@ func (r *DNSPolicyReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		Watches(
 			&source.Kind{Type: &clusterv1.ManagedCluster{}},
 			handler.EnqueueRequestsFromMapFunc(clusterEventMapper.MapToPolicy),
+		).
+		Watches(
+			&source.Kind{Type: &v1alpha1.DNSHealthCheckProbe{}},
+			handler.EnqueueRequestsFromMapFunc(probeEventMapper.MapToPolicy),
 		).
 		Complete(r)
 }