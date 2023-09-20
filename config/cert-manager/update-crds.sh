af5a7d32388d325f92c987f6a7b8f5c7893c4221
Michael Nairn <mnairn@redhat.com>
2023-09-20 11:09:59 +0100

    Rename config/cert-manager/crd/v1.7.1 -> config/cert-manager/crd/latest Remove crd/v1.12.2 and update script Update crds (v1.12.2)

--- config/cert-manager/update-crds.sh
+++ config/cert-manager/update-crds.sh
@@ -7,7 +7,4 @@ set -e pipefail
 chartName=cert-manager
 chartVersion=$(yq ".helmCharts[] | select(.name == \"$chartName\")".version ${SCRIPT_DIR}/kustomization.yaml)
 
-mkdir -p $SCRIPT_DIR/crd/$chartVersion
-curl -L -o $SCRIPT_DIR/crd/$chartVersion/cert-manager.crds.yaml https://github.com/cert-manager/cert-manager/releases/download/$chartVersion/cert-manager.crds.yaml
-
-# Update any reference to the crds in tests i.e. test/integration/suite_test.go, to use the new version.
+curl -L -o $SCRIPT_DIR/crd/latest/cert-manager.crds.yaml https://github.com/cert-manager/cert-manager/releases/download/$chartVersion/cert-manager.crds.yaml
