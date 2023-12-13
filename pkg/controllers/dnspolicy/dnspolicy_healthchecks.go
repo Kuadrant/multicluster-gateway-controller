998784be20650b8f4cf83f4aebf603c71ec81a8a
Phil Brookes <pbrookes@redhat.com>
2023-12-13 16:56:37 +0100

    parse gateway IPs in non-kuadrant format

--- pkg/controllers/dnspolicy/dnspolicy_healthchecks.go
+++ pkg/controllers/dnspolicy/dnspolicy_healthchecks.go
@@ -18,6 +18,7 @@ import (
 
 	"github.com/Kuadrant/multicluster-gateway-controller/pkg/_internal/slice"
 	"github.com/Kuadrant/multicluster-gateway-controller/pkg/apis/v1alpha1"
+	"github.com/Kuadrant/multicluster-gateway-controller/pkg/utils"
 )
 
 func (r *DNSPolicyReconciler) reconcileHealthCheckProbes(ctx context.Context, dnsPolicy *v1alpha1.DNSPolicy, gwDiffObj *reconcilers.GatewayDiff) error {
@@ -122,52 +123,58 @@ func (r *DNSPolicyReconciler) expectedHealthCheckProbesForGateway(ctx context.Co
 		interval = *dnsPolicy.Spec.HealthCheck.Interval
 	}
 
-	for _, address := range gw.Status.Addresses {
-		matches := strings.Split(address.Value, "/")
-		if len(matches) != 2 {
-			log.V(3).Info(fmt.Sprintf("%s cannot be processed. expected <clsutername>/<hostname/ipaddress>", address.Value))
-			return nil
+	gatewayWrapper := utils.NewGatewayWrapper(gw.Gateway)
+	if err := gatewayWrapper.Validate(); err != nil {
+		return nil
+	}
+
+	clusterGatewayAddresses := gatewayWrapper.GetClusterGatewayAddresses()
+
+	for _, listener := range gw.Spec.Listeners {
+
+		//skip wildcard listeners
+		if strings.Contains(string(*listener.Hostname), "*") {
+			continue
 		}
 
-		for _, listener := range gw.Spec.Listeners {
-			if strings.Contains(string(*listener.Hostname), "*") {
-				continue
-			}
+		port := dnsPolicy.Spec.HealthCheck.Port
+		if port == nil {
+			listenerPort := int(listener.Port)
+			port = &listenerPort
+		}
 
-			port := dnsPolicy.Spec.HealthCheck.Port
-			if port == nil {
-				listenerPort := int(listener.Port)
-				port = &listenerPort
-			}
+		var protocol string
+		// handle protocol being nil
+		if dnsPolicy.Spec.HealthCheck.Protocol == nil {
+			protocol = string(listener.Protocol)
+		} else {
+			protocol = string(*dnsPolicy.Spec.HealthCheck.Protocol)
+		}
 
-			// handle protocol being nil
-			var protocol string
-			if dnsPolicy.Spec.HealthCheck.Protocol == nil {
-				protocol = string(listener.Protocol)
-			} else {
-				protocol = string(*dnsPolicy.Spec.HealthCheck.Protocol)
+		for _, addresses := range clusterGatewayAddresses {
+			for _, address := range addresses {
+				log.V(1).Info("reconcileHealthCheckProbes: adding health check for target", "target", address.Value)
+				healthCheck := &v1alpha1.DNSHealthCheckProbe{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      dnsHealthCheckProbeName(address.Value, gw.Name, string(listener.Name)),
+						Namespace: gw.Namespace,
+						Labels:    commonDNSRecordLabels(client.ObjectKeyFromObject(gw), client.ObjectKeyFromObject(dnsPolicy)),
+					},
+					Spec: v1alpha1.DNSHealthCheckProbeSpec{
+						Port:                     *port,
+						Host:                     string(*listener.Hostname),
+						Address:                  address.Value,
+						Path:                     dnsPolicy.Spec.HealthCheck.Endpoint,
+						Protocol:                 v1alpha1.HealthProtocol(protocol),
+						Interval:                 interval,
+						AdditionalHeadersRef:     dnsPolicy.Spec.HealthCheck.AdditionalHeadersRef,
+						FailureThreshold:         dnsPolicy.Spec.HealthCheck.FailureThreshold,
+						ExpectedResponses:        dnsPolicy.Spec.HealthCheck.ExpectedResponses,
+						AllowInsecureCertificate: dnsPolicy.Spec.HealthCheck.AllowInsecureCertificates,
+					},
+				}
+				healthChecks = append(healthChecks, withGatewayListener(gw, listener, healthCheck))
 			}
-			log.V(1).Info("reconcileHealthCheckProbes: adding health check for target", "target", address.Value)
-			healthCheck := &v1alpha1.DNSHealthCheckProbe{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      dnsHealthCheckProbeName(matches[1], gw.Name, string(listener.Name)),
-					Namespace: gw.Namespace,
-					Labels:    commonDNSRecordLabels(client.ObjectKeyFromObject(gw), client.ObjectKeyFromObject(dnsPolicy)),
-				},
-				Spec: v1alpha1.DNSHealthCheckProbeSpec{
-					Port:                     *port,
-					Host:                     string(*listener.Hostname),
-					Address:                  matches[1],
-					Path:                     dnsPolicy.Spec.HealthCheck.Endpoint,
-					Protocol:                 v1alpha1.HealthProtocol(protocol),
-					Interval:                 interval,
-					AdditionalHeadersRef:     dnsPolicy.Spec.HealthCheck.AdditionalHeadersRef,
-					FailureThreshold:         dnsPolicy.Spec.HealthCheck.FailureThreshold,
-					ExpectedResponses:        dnsPolicy.Spec.HealthCheck.ExpectedResponses,
-					AllowInsecureCertificate: dnsPolicy.Spec.HealthCheck.AllowInsecureCertificates,
-				},
-			}
-			healthChecks = append(healthChecks, withGatewayListener(gw, listener, healthCheck))
 		}
 	}
 
