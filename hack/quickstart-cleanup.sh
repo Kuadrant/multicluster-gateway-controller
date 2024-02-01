a72868fa0fb1754fa13e71fa0e97da9284fd2663
Tomas Repel <trepel@redhat.com>
2024-02-01 17:42:26 +0100

    Walkthrough text updates and quickstart cleanup fix

--- hack/quickstart-cleanup.sh
+++ hack/quickstart-cleanup.sh
@@ -3,4 +3,5 @@ export KIND_CLUSTER_PREFIX="mgc-"
 
 source /dev/stdin <<< "$(curl -s https://raw.githubusercontent.com/Kuadrant/multicluster-gateway-controller/main/hack/.cleanupUtils)"
 
-cleanup
+cleanupMGC
+cleanupKind
